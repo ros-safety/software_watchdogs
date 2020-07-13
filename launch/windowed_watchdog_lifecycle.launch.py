20480de2d039af2d7c6b68b3bc2edd5b63696069
Philipp Robbel <robbel@gmail.com>
2020-07-13 17:06:29 -0400

    Repo cleanup.

--- launch/windowed_watchdog_lifecycle.launch.py
+++ launch/windowed_watchdog_lifecycle.launch.py
@@ -12,21 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# from launch import LaunchDescription
-# from launch_ros.actions import LifecycleNode
-# from launch_ros.actions import Node
-##or:
 import launch
 import launch_ros
 import lifecycle_msgs.msg
-
-# def generate_launch_description():
-#     return LaunchDescription([
-#         LifecycleNode(package='lifecycle', node_executable='lifecycle_talker',
-#                       node_name='lc_talker', output='screen'),
-#         Node(package='lifecycle', node_executable='lifecycle_listener', output='screen'),
-#         Node(package='lifecycle', node_executable='lifecycle_service_client', output='screen')
-#     ])
 
 def generate_launch_description():
     set_tty_launch_config_action = launch.actions.SetLaunchConfiguration("emulate_tty", "True")
@@ -40,9 +28,3 @@ def generate_launch_description():
         #arguments=['__log_level:=debug']
     )
     return launch.LaunchDescription([set_tty_launch_config_action, watchdog_node])
-
-#     configure_trans_event = launch.actions.EmitEvent(event=launch_ros.events.lifecycle.ChangeState(
-#         lifecycle_node_matcher=launch_ros.events.lifecycle.matches_node_name('/wiimote'),
-#         transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE
-#     ))
-#     return launch.LaunchDescription([set_tty_launch_config_action, watchdog_node, configure_trans_event])
