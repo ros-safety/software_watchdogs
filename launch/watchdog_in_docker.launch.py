bca3463bddf8415098143a33b463f8d598926033
Philipp Robbel <robbel@gmail.com>
2020-10-20 23:31:31 -0400

    Adding watchdog_in_docker launch file example.

---
+++ launch/watchdog_in_docker.launch.py
@@ -0,0 +1,113 @@
+# Copyright (c) 2020 Mapless AI, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import launch
+from launch import LaunchDescription
+from launch.actions import EmitEvent
+from launch.actions import LogInfo
+from launch.actions import RegisterEventHandler
+from launch_ros.actions import Node
+from launch_ros.actions import LifecycleNode
+from launch_ros.events.lifecycle import ChangeState
+from launch_ros.event_handlers import OnStateTransition
+
+import lifecycle_msgs.msg
+
+def generate_launch_description():
+    set_tty_launch_config_action = launch.actions.SetLaunchConfiguration("emulate_tty", "True")
+
+    # Launch Description
+    ld = launch.LaunchDescription()
+
+    # Start monitored entity once (via docker)
+    docker_run_cmd = launch.actions.ExecuteProcess(
+        cmd=['docker', 'run', '-d', '--name', 'talker', '-v', '/var/run/docker.sock:/var/run/docker.sock', '-v', '/usr/bin/docker:/usr/bin/docker', 'sw_watchdogs:latest', 'ros2', 'launch', 'sw_watchdog', 'heartbeat_composition.launch.py'],
+        #output='screen'
+    )
+
+    # Restart monitored entity upon watchdog miss (via docker)
+    docker_restart_cmd = launch.actions.ExecuteProcess(
+        cmd=['docker', 'restart', 'talker']
+        #output='screen'
+    )
+
+    # Remove docker container
+    docker_rm_cmd = launch.actions.ExecuteProcess(
+        cmd=['docker', 'rm', 'talker']
+        #output='screen'
+    )
+
+    # Shutdown event
+    #shutdown_event = EmitEvent( event = launch.events.Shutdown() )
+
+    # Watchdog node
+    watchdog_node = LifecycleNode(
+        package='sw_watchdog',
+        node_executable='simple_watchdog',
+        node_namespace='',
+        node_name='simple_watchdog',
+        output='screen',
+        arguments=['220', '--publish', '--activate']
+        #arguments=['__log_level:=debug']
+    )
+
+    # Make the Watchdog node take the 'activate' transition
+    watchdog_activate_trans_event = EmitEvent(
+        event = ChangeState(
+            lifecycle_node_matcher = launch.events.process.matches_action(watchdog_node),
+            transition_id = lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE,
+         )
+    )
+
+    # When the watchdog reaches the 'inactive' state, log a message
+    # and restart monitored entity (via docker)
+    watchdog_inactive_handler = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node = watchdog_node,
+            goal_state = 'inactive',
+            entities = [
+                # Log
+                LogInfo( msg = "Watchdog transitioned to 'INACTIVE' state." ),
+                # Change state event (inactive -> active)
+                watchdog_activate_trans_event,
+                # Restart the monitored entity
+                docker_restart_cmd,
+            ],
+        )
+    )
+
+    # When the Watchdog node reaches the 'finalized' state, clean up docker
+    watchdog_finalized_state_handler = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node = watchdog_node,
+            goal_state = 'finalized',
+            entities = [
+                # Log
+                LogInfo( msg = "Watchdog transitioned to 'FINALIZED' state." ),
+                # Clean up docker
+                docker_rm_cmd,
+                #shutdown_event,
+            ],
+        )
+    )
+
+    # Add the actions to the launch description.
+    # The order they are added reflects the order in which they will be executed
+    ld.add_action( set_tty_launch_config_action )
+    ld.add_action( docker_run_cmd )
+    ld.add_action( watchdog_node )
+    ld.add_action( watchdog_inactive_handler )
+    ld.add_action( watchdog_finalized_state_handler )
+
+    return ld
